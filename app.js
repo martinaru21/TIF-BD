b1060ba36bbc273cbfcc7e6e8b8afec3a621644b
FacuCP <facucp102@gmail.com>
2023-11-17 16:43:48 -0300

    DeveloperInsert

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const port = 3000;
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'edinburgo2030',
+    password: '2002',
     database: 'tif',
 });
 
@@ -56,9 +56,9 @@ connection.connect((err) => {
       <button onclick="location.href='/bugMenu'">Bugs</button>
       <button onclick="location.href='/sedeMenu'">Sedes</button>
       <button onclick="location.href='/equipMenu'">Equipamiento</button>
-      <button onclick="location.href='/devMenu'">Developers</button>
+      <button onclick="location.href='/developerMenu'">Developers</button>
       <button onclick="location.href='/designerMenu'">Designers</button>
-      <button onclick="location.href='/artistMenu'">Artistas</button>
+      <button onclick="location.href='/artistaMenu'">Artistas</button>
       <button onclick="location.href='/testerMenu'">Testers</button>
       <button onclick="location.href='/assetMenu'">Assets</button>
       <button onclick="location.href='/featMenu'">Features</button>
@@ -100,12 +100,12 @@ connection.connect((err) => {
       });
 
   //*************MENU DEVELOPERS*************
-  app.get('/devMenu', (req, res) => {
+  app.get('/developerMenu', (req, res) => {
     res.send(`
       <h1>Administrador: Developers</h1>
-      <button onclick="location.href='/sedeInsertForm'">Nuevo</button>
-      <button onclick="location.href='/sedeInsertForm'">Modificar</button>
-      <button onclick="location.href='/sedeInsertForm'">Insertar</button>
+      <button onclick="location.href='/developerInsForm'">Nuevo</button>
+      <button onclick="location.href='/developerModForm'">Modificar</button>
+      <button onclick="location.href='/developerDelForm'">Eliminar</button>
     `);
   });
 
@@ -113,13 +113,31 @@ connection.connect((err) => {
   app.get('/designerMenu', (req, res) => {
     res.send(`
       <h1>Administrador: Designers</h1>
-      <button onclick="location.href='/sedeInsertForm'">Nuevo</button>
-      <button onclick="location.href='/sedeInsertForm'">Modificar</button>
-      <button onclick="location.href='/sedeInsertForm'">Insertar</button>
+      <button onclick="location.href='/designerInsForm'">Nuevo</button>
+      <button onclick="location.href='/designerModForm'">Modificar</button>
+      <button onclick="location.href='/designerDelForm'">Eliminar</button>
     `);
   });
 
+//*************MENU ARTISTAS*************
+app.get('/artistaMenu', (req, res) => {
+  res.send(`
+    <h1>Administrador: Artistas</h1>
+    <button onclick="location.href='/sedeInsertForm'">Nuevo</button>
+    <button onclick="location.href='/sedeInsertForm'">Modificar</button>
+    <button onclick="location.href='/sedeInsertForm'">Insertar</button>
+  `);
+});
 
+//*************MENU TESTERS*************
+app.get('/testerMenu', (req, res) => {
+  res.send(`
+    <h1>Administrador: Tester</h1>
+    <button onclick="location.href='/sedeInsertForm'">Nuevo</button>
+    <button onclick="location.href='/sedeInsertForm'">Modificar</button>
+    <button onclick="location.href='/sedeInsertForm'">Insertar</button>
+  `);
+});
 
 
 //*************MOSTRAR TABLAS*************
@@ -333,13 +351,30 @@ app.get('/showDevTable', (req, res) => {
 //*************INSERTS*************
   // Serve the HTML form for inserting a SEDE
   app.get('/sedeInsertForm', (req, res) => {
-    res.sendFile(__dirname + '/sedeInsertForm.html'); // Provide the path to your sedeInsertForm.html file
+    res.sendFile(__dirname + '\\views/sedeInsertForm.html'); // Provide the path to your sedeInsertForm.html file
   });
   
   // Handle the form submission for inserting a SEDE
   app.post('/insertSede', async (req, res) => {
     const { cuit, nombre_sede, direccion, ciudad, provincia, pais, codigoPostal, alquiler, capacidad } = req.body;
     connection.query("call crearSede(?, ?, ?, ?, ?, ?, ?, ?, ?)", [cuit, nombre_sede, direccion, ciudad, provincia, pais, codigoPostal, alquiler, capacidad], function (err, result){
+        if (err) {
+            console.log("err:", err);
+        } else {
+            console.log("results:", result);
+        }
+    });
+  });
+
+    // Serve the HTML form for inserting a Developer
+  app.get('/developerInsForm', (req, res) => {
+    res.sendFile(__dirname + '\\views/developerInsForm.html'); // Provide the path to your sedeInsertForm.html file
+  });
+  
+  // Handle the form submission for inserting a SEDE
+  app.post('/insertDeveloper', async (req, res) => {
+    const { dni, nombre, apellido, fecha_nac, ing_emp, ing_proy, sueldo, rubro, seniority, grupo } = req.body;
+    connection.query("call crearDeveloper(?,?,?,?,?,?,?,?,?,?)", [dni, nombre, apellido, fecha_nac, ing_emp, ing_proy, sueldo, rubro, seniority, grupo], function (err, result){
         if (err) {
             console.log("err:", err);
         } else {
